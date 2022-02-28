3bc00043638ebe0e935364c01b56921a1dda668a
Dolicon <dthx2710@hotmail.com>
2022-03-01 03:41:15 +0800

    Source Code wk6

---
+++ Qn123.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Qn123 {
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+        llist.add(1);
+        llist.add(3);
+        llist.add(5);
+        llist.add(7);
+        llist.add(9);
+        llist.add(11);
+        System.out.println("Before: " + llist);
+        addAndSort(llist, 6);
+        System.out.println("After addAndSort: " + llist);
+        swapValues(llist, 1, 5);
+        System.out.println("After swapValues: " + llist);
+        System.out.println("Random LinkedList Index: " + randomLinkedList());
+    }
+    public static void addAndSort(LinkedList llist, int v){
+        // if new value is bigger, simply append to list
+        int size = llist.size();
+        if ((int)llist.get(size-1)<v){
+            llist.add(v);
+        }
+        // bruteforce method: check elements 1 by 1 to see which index position to insert into
+        // O(n) complexity
+        else{
+            int i = 0;
+            while ((int)llist.get(i)<v){
+                ++i;
+            }
+            llist.add(i, v);
+        }
+    }
+    public static void swapValues(LinkedList llist, int v1, int v2){
+        int index1 = llist.indexOf(v1);
+        int index2 = llist.indexOf(v2);
+        if (index1 == -1 || index2 == -1){
+            return;
+        }
+        // swap set
+        llist.set(index1, v2);
+        llist.set(index2, v1);
+    }
+    public static int randomLinkedList(){
+        Random rand = new Random();
+        LinkedList llist = new LinkedList();
+        int index = -1;
+        // generate 500 random integers
+        for (int i=0;i<500;++i){
+            // add 1000-9999, in random order
+            llist.add(rand.nextInt(1000, 10000));
+        }
+        int randInt = rand.nextInt(1000,10000);
+        for (int i=0;i<500;++i){
+            if ((int)llist.get(i)==randInt){
+                index = i;
+            }
+        }
+        return index;
+    }
+}
---
+++ Qn456.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Qn456 {
+    public static void main(String[] args) {
+        HashMap hm = new HashMap();
+        hm.put(0, 1);
+        hm.put(1, 3);
+        hm.put(2, 5);
+        hm.put(3, 7);
+        hm.put(4, 9);
+        hm.put(5, 11);
+        System.out.println("Before: " + hm);
+        putAndSort(hm, 6);
+        System.out.println("After putAndSort: " + hm);
+        swapValues(hm, 1, 5);
+        System.out.println("After swapValues: " + hm);
+        System.out.println("Random HashMap Index: " + randomHashMap());
+    }
+    public static void putAndSort(HashMap hm, int v){
+        // if new value is bigger, simply append element to hmap
+        int size = hm.size();
+        if ((int)hm.get(size-1)<v){
+            hm.put(size, v);
+        }
+        else{
+            int i = 0;
+            // find index to insert
+            while ((int)hm.get(i)<v){
+                ++i;
+            }
+            // check starting from index i
+            // move all other above elements' key  up by 1 (to make space for new element), starting from top down
+            // top is index of last element, we make top element key up by 1 (e.g. 5=11 -> 6=11)
+            int top = size-1;
+            for (int j=i;j<size;++j){
+                hm.put(top+1,hm.get(top));
+                --top;
+            }
+            hm.put(i, v);
+        }
+    }
+    public static void swapValues(HashMap hm, int v1, int v2){
+        int tempStore1 = (int)hm.get(v1);
+        int tempStore2 = (int)hm.get(v2);
+
+        // remove and put to swap
+        hm.remove(v1);
+        hm.put(v1, tempStore2);
+        hm.remove(v2);
+        hm.put(v2, tempStore1);
+    }
+    public static int randomHashMap(){
+        Random rand = new Random();
+        HashMap hm = new HashMap();
+        AtomicInteger randIndex = new AtomicInteger(-1);
+        // generate 500 random integers
+        for (int i=0;i<500;++i){
+            // put 1000-9999, in random order
+            hm.put(i, rand.nextInt(1000, 10000));
+        }
+        int randInt = rand.nextInt(1000,10000);
+        if (hm.containsValue(randInt)){
+            hm.forEach((k,v)->{
+               if ((int)v==randInt){
+                   randIndex.set((int)k);
+               }
+            });
+        }
+        return randIndex.get();
+    }
+}
